251f1cee07e2ae68b2adc8c35413a859ab7feb13
minas <minas@MelidoniAbroad.fritz.box>
2024-02-04 23:35:29 +0100

    Final Clean model

--- mobile basis/Three_wheeled_base/simscape_1/slprj/raccel/Three_wheeled_base/Three_wheeled_base_types.h
+++
@@ -1,9 +0,0 @@
-#ifndef RTW_HEADER_Three_wheeled_base_types_h_
-#define RTW_HEADER_Three_wheeled_base_types_h_
-#ifndef SS_UINT64
-#define SS_UINT64 17
-#endif
-#ifndef SS_INT64
-#define SS_INT64 18
-#endif
-#endif
--- mobile basis/Three_wheeled_base/simscape_1/slprj/raccel/Three_wheeled_base/setup_msvc.bat
+++
@@ -1,2 +0,0 @@
-set "VSCMD_START_DIR=%CD%"
-CALL "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\VCVARSALL.BAT " amd64
